ebd7b398b2bcf7e163184c08ef19375e0efaa2ff
tanosgabi <tanosgabi@gmail.com>
2015-02-23 23:40:50 +0100

    mostmár statikus is

--- stringcalculator/src/main/java/main/StringCalculator.java
+++ stringcalculator/src/main/java/main/StringCalculator.java
@@ -1,3 +1,5 @@
+package main;
+
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
@@ -9,7 +11,7 @@
  * @author Gábor
  */
 public class StringCalculator {
-    int add(String numbers)
+    public static int add(String numbers)
     {
         int osszeg = 0;
         if(numbers.isEmpty())
