f6422263b0d004b27e24ae2908b92963c47257cf
tanosgabi <tanosgabi@gmail.com>
2015-02-24 00:03:30 +0100

    Végső megoldás

--- stringcalculator/src/main/java/main/StringCalculator.java
+++ stringcalculator/src/main/java/main/StringCalculator.java
@@ -1,5 +1,8 @@
 package main;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
@@ -12,18 +15,20 @@ package main;
  */
 public class StringCalculator {
     private static String elvalaszto=",";
-    public static int add(String numbers)
+    public static int add(String numbers) throws Exception
     {
         int osszeg = 0;
-        if(numbers.isEmpty())
-            osszeg=0;
-        else
-        {
-            String[] szamok=numbers.split(elvalaszto);
-            for (String szamok1 : szamok) {
+        String negativ="";
+        String[] szamok=numbers.split(elvalaszto);
+        for (String szamok1 : szamok) {     
+            if(!szamok1.isEmpty()){
                 osszeg+=Integer.parseInt(szamok1);
-            }
+                if(Integer.parseInt(szamok1)<0)negativ+=szamok1+",";
+            }else
+                osszeg+=0;
         }
+        if(!negativ.isEmpty())
+            throw new Exception("“Negatives not allowed: "+negativ);
         return osszeg;
     }
 
